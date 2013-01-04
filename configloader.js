583890c9a85f58764d50efe7e06e4bc4a3cc4df8
Simon Ljungberg <hi@iamsim.me>
2013-01-04 18:32:48 +0100

    Code cleanup

--- configloader.js
+++ configloader.js
@@ -4,10 +4,10 @@
 // Loads configuration files based on the current environment.
 //
 // * _path_: path to the directory containing configuration files.
-// * _options_:
+// * _options_: optional optionas object, for now only has "env" if you'd like
+// to load configs for a specific environment.
 // * _fn_: optional callback. If left out the configs will be loaded sync.
 //
-var _       = require('lodash');
 var p       = require('path');
 var yaml    = require('js-yaml');
 var fs      = require('fs');
@@ -24,9 +24,72 @@ module.exports = function (path, options, fn) {
   var env   = options.env || process.env.NODE_ENV || 'dev';
   options.env = env;
 
-  if (async) loadAsync(path, options, fn);
+  if (async)  loadAsync(path, options, fn);
   else return loadSync(path, options);
 };
+
+//
+// ## Load sync
+//
+// Loads configurations sync.
+// Throws if no configuration files are found.
+//
+function loadSync(path, options) {
+  var paths = buildPaths(path, options);
+  var base  = false;
+  var env   = false;
+
+  try { base = require(paths.p1); } catch (err) { }
+  try { env  = require(paths.p2); } catch (err) { }
+
+  if (base && env)        return merge(base, env);
+  else if (!base && env)  return env;
+  else if (!base && !env) throw new Error('No configuration files found.');
+  else                    return base;
+}
+
+//
+// ## Load async
+//
+// Load configuration files async.
+// Stops loading if an error is encountered.
+//
+function loadAsync(path, options, fn) {
+  var paths = buildPaths(path, options);
+  var files = {};
+  files[paths.p1] = false;
+  files[paths.p2] = false;
+
+  // @TODO: perhaps shouldn't rely on everything being UTF-8...
+  var parsed = [];
+  filemap(files, 'utf-8', function (result) {
+    var err;
+    for (var i in result) {
+      if (result[i]) {
+        try { parsed.push(yaml.load(result[i])); }
+        catch (error) {
+          err = error;
+          break;
+        }
+      }
+    }
+
+    if (parsed.length == 1) fn(err, parsed[0]);
+    else                    fn(err, merge(parsed[0], parsed[1]));
+  });
+}
+
+//
+// ## Build paths
+//
+// Returns an object containing paths to base and env configs.
+//
+function buildPaths(path, options) {
+  return {
+    p1: p.join(path, 'config.yml'),
+    p2: p.join(path, 'config.' + options.env + '.yml')
+  };
+}
 
 // Deep extend.
 // From http://andrewdupont.net/2009/08/28/deep-extending-objects-in-javascript/
@@ -41,54 +104,5 @@ function merge(destination, source) {
     }
   }
   return destination;
-}
-
-// Load sync
-function loadSync(path, options) {
-  var paths = buildPaths(path, options);
-  var base  = require(paths.p1);
-  var env   = false;
-
-  try { base = require(paths.p1); } catch (err) {}
-  try { env  = require(paths.p2); } catch (err) {}
-
-  if (env && base)        return merge(base, env);
-  else if (!base && env)  return env;
-  else                    return base;
-}
-
-// Load async
-function loadAsync(path, options, fn) {
-  var paths = buildPaths(path, options);
-  var files = {};
-  files[paths.p1] = false;
-  files[paths.p2] = false;
-
-  // @TODO: perhaps shouldn't rely on everything being UTF-8...
-  filemap(files, 'utf-8', function (result) {
-    var parsed = [];
-    var err;
-    for (var i in result) {
-      if (result[i]) {
-        try {
-          parsed.push(yaml.load(result[i]));
-        } catch (error) {
-          err = error;
-          break;
-        }
-      }
-    }
-
-    if (parsed.length == 1) fn(err, parsed[0]);
-    else                    fn(err, merge(parsed[0], parsed[1]));
-  });
-}
-
-// Build paths
-function buildPaths(path, options) {
-  return {
-    p1: p.join(path, 'config.yml'),
-    p2: p.join(path, 'config.' + options.env + '.yml')
-  };
 }
 
